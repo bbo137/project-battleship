703c8cf490e60eca81a559ebb1127ca4272b38b2
Bribb0L <50684429+Bribb0L@users.noreply.github.com>
2023-07-01 08:12:25 -0400

    Add ship factory function -hit: stores hitcount -getHits: return hitcoun -isSunk: check if ship is sunk -add tests the ship functions

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,14 @@
-function Ship(length, hits = 0, sunk = false) {
-    return { length, hits, sunk };
+function Ship(length) {
+	let hits = 0;
+	const hit = () => {
+		hits +=1;
+	}
+
+	const getHits = () => hits;
+
+	const isSunk = () => (length - hits) <= 0;
+
+	return { length, hits, hit, getHits, isSunk};
 }
 
 
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -1,8 +1,31 @@
 import { Ship } from "../src/ship"
 
 
-test('expect string to be String', () => {
+test.only('increase hits by 1 when ship is hit once', () => {
     const testShip = Ship(3);
-    testShip.hits();
-    expect(testShip.getHitNum().toBe(1));
+    testShip.hit();
+    expect(testShip.getHits()).toBe(1);
+  });
+
+  test.only('increase hits by 2 when ship is hit twice', () => {
+    const testShip = Ship(3);
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.getHits()).toBe(2);
+  });
+
+  test.only('expect true when ship is sunk', () => {
+    const testShip = Ship(3);
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(true);
+  });
+
+  test.only('expect false when ship is sunk', () => {
+    const testShip = Ship(6);
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(false);
   });
